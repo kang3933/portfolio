fb811dee27baf367c6801d1b24cca547306b5c58
henry kang <henryk3933@gmail.com>
2025-04-19 05:52:36 +1000

    project img hover effect

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       {/* <Header/> */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
